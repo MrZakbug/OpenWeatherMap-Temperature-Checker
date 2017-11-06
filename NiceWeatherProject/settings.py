0e6b8da35ecbadfc10af916c85ce52c0a1934025
mrzakbug <mrzakbug@gmail.com>
2017-11-06 14:59:57 +0100

    Debug to True

--- NiceWeatherProject/settings.py
+++ NiceWeatherProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@p+m&=-t)9d!()mdx9jwfo)m_hrlwtzd)_i-^nw=)&1i9!q#53'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '127.0.0.1:8000']
 
